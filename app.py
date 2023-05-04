f0adcc700fa0d9cc78a902ab5d8680027592dcbc
Waldemar Bautista <waldemar@pearlpay.io>
2023-05-04 17:00:51 +0800

    Cleanup

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pymysql
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 
@@ -15,13 +16,11 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    db.session.query(text('1')).from_statement(text('SELECT 1')).all()
-    return "<h1>It works.</h1>Version: {}".format(os.environ['VERSION'])
-    #try:
-    #    db.session.query(text('1')).from_statement(text('SELECT 1')).all()
-    #    return "<h1>It works.</h1>Version: {}".format(os.environ['VERSION'])
-    #except:
-    #    return "<h1>Something is broken.</h1>Version: {}".format(os.environ['VERSION'])
+    try:
+        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+        return "<h1>Database connection works.</h1>Version: {}".format(os.environ['VERSION'])
+    except:
+        return "<h1>Something is broken.</h1>Version: {}".format(os.environ['VERSION'])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
